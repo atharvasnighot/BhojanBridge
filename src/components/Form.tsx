e7c904b8de5feb913395b1d755356641bd48bee0
KaKashi1210 <desmondaditya@gmail.com>
2023-10-12 18:40:12 +0530

    feat: added multi routes

---
+++ src/components/Form.tsx
@@ -0,0 +1,57 @@
+export default () => {
+  return (
+    <>
+      <nav className="p-4 bg-blue-500 items-center">
+        <div className="container mx-auto items-center">
+
+          <div className="max-w-md mx-auto mt-8">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" >
+                  Username
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="username"
+                  type="text"
+                  placeholder="Enter your username"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Email
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  placeholder="Enter your email"
+                />
+              </div>
+              <div className="mb-6">
+                <label className="block text-gray-700 text-sm font-bold mb-2" >
+                  Password
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="Enter your password"
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Sign Up
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+    </>
+  )
+}
---
+++ src/page/Page1.tsx
@@ -0,0 +1,17 @@
+import BelowCarousal from "../components/BelowCarousal"
+import Carousal from "../components/Carousal"
+import Header from "../components/Header"
+import Map from "../components/Map"
+
+export default () => {
+
+  return (
+    <>
+      <Header />
+      <Carousal />
+      <BelowCarousal />
+      <Map />
+    </>
+  )
+}
+
