a4eca94de9de05506dc0cc9da4d96c6d72841024
Nikhil Kamble <nikhilkamblestudy@gmail.com>
2023-10-13 07:51:43 +0530

    Profile added & assets uploaded

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -5,23 +5,26 @@ export default () => {
   return (
     <>
       <nav className="p-4 bg-transparent items-center">
-        <div className="container mx-auto items-center">
-          <div className="flex justify-between items-center">
-            <a href="/" className="text-black text-2xl font-bold"> FoodBridge</a>
-            <div>
-              <ul className="flex space-x-6">
-                <li><a href="/" className="text-black flex items-center"> <AiOutlineSearch />{"  "}Search</a></li>
-                <li><a href="/" className="text-black flex items-center"><AiFillHome /> Home</a></li>
-                <li><a href="/" className="text-black flex items-center"><AiFillInfoCircle /> About</a></li>
-                <li><a href="/" className="text-black flex items-center"><AiFillContacts /> Contact</a></li>
-                <Link to='/3'>
-                <li><button className="text-zinc-1800 bg-white p-2 rounded-md">Login</button></li>
-                </Link>
-              </ul>
+        <nav className="p-4 bg-rgba(0, 0, 255, 0.2) items-center">
+          <div className="container mx-auto items-center">
+            <div className="flex justify-between items-center">
+              <a href="/" className="text-black text-2xl font-bold"> FoodBridge</a>
+              <div>
+                <ul className="flex space-x-6">
+                  <li><a href="/" className="text-black flex items-center"> <AiOutlineSearch />{"  "}Search</a></li>
+                  <li><a href="/" className="text-black flex items-center"><AiFillHome /> Home</a></li>
+                  <li><a href="/" className="text-black flex items-center"><AiFillHome /> Profile</a></li>
+                  <li><a href="/" className="text-black flex items-center"><AiFillInfoCircle /> About</a></li>
+                  <li><a href="/" className="text-black flex items-center"><AiFillContacts /> Contact</a></li>
+                  <Link to='/3'>
+                    <li><button className="text-zinc-1800 bg-white p-2 rounded-md">Login</button></li>
+                  </Link>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      </nav >
+        </nav >
+      </nav>
     </>
   )
 }
--- Client/src/components/Home.tsx
+++ Client/src/components/Home.tsx
@@ -3,38 +3,46 @@ import {
 } from "@material-tailwind/react";
 import BelowCarousal from "../components/BelowCarousal";
 import Carousal from "../components/Carousal";
-import Header from "../components/Header";
 import Map from "../components/Map";
 
 export default () => {
-  return (
-    <>
-        <Header />
-      <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
-        <div className="absolute top-0 h-full w-full bg-[url('https://img.freepik.com/free-photo/top-view-yellow-rice-pita-arrangement_23-2148728164.jpg?w=1380&t=st=1697160604~exp=1697161204~hmac=1d193e6f9d200d8f3814efe593575f8ea7836883442f280953271cbbc59efa05')] bg-cover bg-center" />
-        <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
-        <div className="max-w-8xl container relative mx-auto">
-          <div className="flex flex-wrap items-center">
-            <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
-              <Typography
-                variant="h1"
-                color="white"
-                className="mb-15 font-black"
-              >
-                Your Journey of Starving Ends with us.
-              </Typography>
-              <Typography variant="lead" color="white" className="opacity-80">
-              Embark on a journey where hunger and food wastage meet their end. 
-              Our app empowers you to make a difference, one meal at a time.
-              </Typography>
+    return (
+        <>
+
+            <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
+                <div className="absolute top-0 h-full w-full bg-[url('https://img.freepik.com/free-photo/top-view-yellow-rice-pita-arrangement_23-2148728164.jpg?w=1380&t=st=1697160604~exp=1697161204~hmac=1d193e6f9d200d8f3814efe593575f8ea7836883442f280953271cbbc59efa05')] bg-cover bg-center" />
+                <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
+                <div className="max-w-8xl container relative mx-auto">
+                    <div className="flex flex-wrap items-center">
+                        <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
+                            <Typography
+                                variant="h1"
+                                color="white"
+                                className="mb-15 font-black"
+                            >
+                                Your Journey of Starving Ends with us.
+                            </Typography>
+                            <Typography variant="lead" color="white" className="opacity-80">
+                                Embark on a journey where hunger and food wastage meet their end.
+                                Our app empowers you to make a difference, one meal at a time.
+                            </Typography>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-      
-      <Carousal />
-      <BelowCarousal />
-      <Map />
-    </>
-  );
+            {/* Section with a different background for Carousal, BelowCarousal, and Map */}
+            <div
+                className="relative"
+                style={{
+                    backgroundImage: `url('https://img.freepik.com/free-photo/indian-food-with-copy-space-high-angle_23-2148747687.jpg?w=360&t=st=1697162843~exp=1697163443~hmac=a649af113ff6c0b045d83be42492f4a2abf3cc054fa975f0b4a7288a2471274b')`,
+                    backgroundSize: "fit",
+                    backgroundPosition: "center",
+                }}
+            >
+                <Carousal />
+                <BelowCarousal />
+                <Map />
+            </div>
+        </>
+    );
 };
